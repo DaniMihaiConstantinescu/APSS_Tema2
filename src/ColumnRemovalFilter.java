f7ae56175859a69fb45df434f7841dcd6baa9753
DaniMihaiConstantinescu <mihai.constantinescu02@e-uvt.ro>
2025-10-29 11:33:34 +0200

    Initial commit

---
+++ src/ColumnRemovalFilter.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+/**
+ * Un filtru de unificare are doua porturi de intrare si un port de iesire.
+ * El transfera la portul de iesire datele disponibile pe oricare din porturile de intrare
+ * fara a le modifica.
+ * NU exista nici o informatie si nici o premiza legate de ]ncarcarea datelor de pe porturile de intrare.
+ * Din acest motiv, cand datele sunt disponibile pe ambele porturi filtrul poate alege
+ * oricare port de intrare.
+ */
+public class ColumnRemovalFilter extends Filter {
+
+    /**
+     * Portul de intrare.
+     **/
+    protected BufferedReader pInput;
+
+    /**
+     * Portul de iesire.
+     **/
+    protected BufferedWriter pOutput;
+
+    /**
+     * Construieste un filtru unificator cu numele dat.
+     * Porturile sunt impachetate intr-un flux de caratere buffer-at.
+     *
+     * @param sName   sirul ce reprezinta numele filtrului
+     * @param pInput1 portul de intrare al acestui filtru
+     * @param pOutput portul de iesire al acestui filtru
+     */
+    public ColumnRemovalFilter(String sName,
+                       BufferedReader pInput1, BufferedWriter pOutput) {
+        // Executarea constructorului clasei parinte.
+        super(sName);
+
+        // Initializarea porturilor de intrare si de iesire.
+        this.pInput = pInput1;
+        this.pOutput = pOutput;
+    }
+
+    /**
+     * Precizeaza daca datele sunt disponibile pe porturile de intrare.
+     *
+     * @return <code>true</code> daca si numai daca acest filtru
+     * poate citi date de la unul din porturile de intrare.
+     * @throws IOException
+     */
+    protected boolean ready() throws IOException {
+        return this.pInput.ready();
+    }
+
+    /**
+     * Citeste date disponibile la unul din porturile de intrare si
+     * scrie date noi la portul de iesire.
+     *
+     * @throws IOException
+     */
+    protected void work() throws IOException {
+
+        // Citirea unei inregistrari de pe portul de intrare selctat.
+        Student objStudent = new Student(pInput.readLine());
+
+        // Scrierea inregistrarii la portul de iesire.
+        this.pOutput.write(String.format("%s %s", objStudent.sName, objStudent.sProgram));
+        this.pOutput.newLine();
+        this.pOutput.flush();
+    }
+}
